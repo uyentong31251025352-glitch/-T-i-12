9624d498ba3ab38138662e763d57c080dce0882e
uyentong31251025352-glitch <uyentong.31251025352@st.ueh.edu.vn>
2026-05-04 12:10:54 +0700

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,30 @@ namespace Đề_Tài_12
 {
     public partial class Form1 : Form
     {
+        PostList postList = new PostList();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        void Duyetdanhsach()
+
+        {
+
+            listBox1.Items.Clear();
+
+            Node current = postList.Head;
+
+            while (current != null)
+
+            {
+
+                listBox1.Items.Add(current.Data.ID + " - " + current.Data.Content);
+
+                current = current.Next;
+
+            }
+
         }
     }
 }
